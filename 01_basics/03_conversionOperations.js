c82748cc4b98d033b7f8a36e3bd7d6d0fface24c
Anshitagupta25 <gupta.anshita1281@gmail.com>
2026-05-29 21:06:29 +0000

    Conversion of data types and types of operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,26 +1,99 @@
-//  let score = 27
-//  let score = "27abc"
- let score = null
-console.log(typeof score);         //will print "number"
-console.log(typeof(score));      //will also print "number"
-let valueInNumber = Number(score)      //Number ek datatype ki trha use kia h over here
-console.log(typeof valueInNumber)   //will print "number"
-console.log(valueInNumber)  //will print 27 which is the value on the score variable
-// now if we will take score as "23abc" as score lets see what it will print.
-// after executng it says its NaN which means not a number because it is not able to convert the string into number as it has some characters in it. so it will print NaN. and the type of NaN is also number. so it will print number as well.
-//NaN ka type b number h
+// //  let score = 27
+// //  let score = "27abc"
+//  let score = null
+// console.log(typeof score);         //will print "number"
+// console.log(typeof(score));      //will also print "number"
+// let valueInNumber = Number(score)      //Number ek datatype ki trha use kia h over here
+// console.log(typeof valueInNumber)   //will print "number"
+// console.log(valueInNumber)  //will print 27 which is the value on the score variable
+// // now if we will take score as "23abc" as score lets see what it will print.
+// // after executng it says its NaN which means not a number because it is not able to convert the string into number as it has some characters in it. so it will print NaN. and the type of NaN is also number. so it will print number as well.
+// //NaN ka type b number h
 
 
-// now lets take the value of score as NULL.
-// when we will execute it, it will print 0 because null is considered as 0 when we will convert it into number. and the type of 0 is also number so it will print number as well. so it will print number and 0.
+// // now lets take the value of score as NULL.
+// // when we will execute it, it will print 0 because null is considered as 0 when we will convert it into number. and the type of 0 is also number so it will print number as well. so it will print number and 0.
 
-let isLoggedIn = 0
-let booleanIsLoggedIn = Boolean(isLoggedIn)  //Boolean is also a datatype like number and string. it is used to convert any value into boolean value. it will return true if the value is truthy and false if the value is falsy.
-console.log(booleanIsLoggedIn)  //will print true because 1 is a truthy value.
-console.log(typeof booleanIsLoggedIn)  //will print boolean because the type of booleanIsLoggedIn is boolean.
+// let isLoggedIn = 0
+// let booleanIsLoggedIn = Boolean(isLoggedIn)  //Boolean is also a datatype like number and string. it is used to convert any value into boolean value. it will return true if the value is truthy and false if the value is falsy.
+// console.log(booleanIsLoggedIn)  //will print true because 1 is a truthy value.
+// console.log(typeof booleanIsLoggedIn)  //will print boolean because the type of booleanIsLoggedIn is boolean.
 
 
-let n = 123
-let stringNumber = String(n)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let n = 123
+// let stringNumber = String(n)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+/// ******** OPERATIONSSS **************
+
+// let value = 3
+// let negValue = -value
+// console.log(value)
+// console.log(negValue)
+
+// console.log(2+2) //will print sum
+// console.log("2"+"2") //will print 22 because when we will add two strings it will concatenate(combine) them instead of adding them. so it will print 22.
+// console.log(2-2) //subtraction
+// console.log(2*2) //multiplication
+// console.log(2/2) //division
+// console.log(2%2)// modulus -> remainder
+// console.log(2**2) //exponentional -> power(Raised power basically)
+// console.log(2+2*3) // output = 8 because of multiplications highest precedence order than other operators.
+
+
+// let str1 = "hello"
+// let str2 = " ANSHI"
+// // let str3 = str1 + str2
+//  str2 = str1 + str2
+// console.log(str2) //will print "hello ANSHI" because when we will add two strings it will concatenate(combine) them instead of adding them. so it will print "hello ANSHI". and there is a space before ANSHI because we have added a space in the str2 variable. if we will not add a space then it will print "helloANSHI" without space. so it is important to add a space if we want to have a space between the two strings.
+
+// console.log(1+2)
+// console.log("1"+ 2)
+// console.log(1+"2")
+// console.log("1" + "2")
+// console.log(1 + 2 + "2") //will print 32 because when we will add 1 and 2 it will give us 3 and then when we will add 3 and "2" it will concatenate them and give us "32". so it will print "32". and the type of "32" is string. so it will print string as well. so it will print "32" and string.
+// console.log("1" + 2 + 3) //will print "123" because when we will add "1" and 2 it will concatenate them and give us "12" and then when we will add "12" and 3 it will concatenate them and give us "123". so it will print "123". and the type of "123" is string. so it will print string as well. so it will print "123" and string.  
+
+
+// console.log(+true); //will print 1 because when we will convert true into number it will give us 1. and the type of 1 is number. so it will print number as well. so it will print 1 and number.
+// console.log(+false); //will print 0 because when we will convert false into number it will give us 0. and the type of 0 is number. so it will print number as well. so it will print 0 and number.
+// console.log(+""); //will print 0 because when we will convert empty string into number it will give us 0. and the type of 0 is number. so it will print number as well. so it will print 0 and number.
+// console.log(+"hello"); //will print NaN because when we will convert "hello" into number it will give us NaN because it is not a valid number. and the type of NaN is number. so it will print number as well. so it will print NaN and number.              
+
+//prefix increment and decrement
+
+// let num = 5
+// console.log(++num) //will print 6 because when we will use prefix increment operator it will first increment the value of num and then return the incremented value. so it will print 6. and the value of num will be 6 after this operation.
+// console.log(num) //will print 6 because the value of num is now 6 after the previous operation.
+
+// let num2 = 5
+// console.log(--num2) //will print 4 because when we will use prefix decrement operator it will first decrement the value of num2 and then return the decremented value. so it will print 4. and the value of num2 will be 4 after this operation.
+// console.log(num2) //will print 4 because the value of num2 is now 4 after the previous operation.
+
+//postfix increment and decrement
+// let num3 = 5 
+// console.log(num3++) //will print 5 because when we will use postfix increment operator it will first return the current value of num3 and then increment the value of num3. so it will print 5. and the value of num3 will be 6 after this operation.
+// console.log(num3) //will print 6 because the value of num3 is now 6 after the previous operation.
+
+// let num4 = 5
+// console.log(num4--) //will print 5 because when we will use postfix decrement operator it will first return the current value of num4 and then decrement the value of num4. so it will print 5. and the value of num4 will be 4 after this operation.
+// console.log(num4) //will print 4 because the value of num4 is now 4 after the previous operation.    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
